9fcd836b3fef22fd1374f3f1dd4c1e567e8efd57
qSlnz <hub@raffenne.fr>
2020-12-06 02:59:37 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ sudo apt-get upgrade
 sudo apt-get install emacs tree zsh -y
 
 # Installation et configuration de zsh / oh-my-zsh
-echo "zsh" >> ~/.bashrc
+echo "cd" >> ~/.bashrc
 sh -c "$(wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
 cp silenz.zsh-theme ~/.oh-my-zsh/themes
 sed -i 's/ZSH_THEME=.*/ZSH_THEME="silenz"/' ~/.zshrc
