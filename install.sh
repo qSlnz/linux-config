d8dbfcc8abeb4a2e8e0423fc439f2b6ce5697bc1
s7z <hub@raffenne.fr>
2021-12-04 15:44:43 +0100

    Adding 'cd' to zshrc + autosource at end

--- install.sh
+++ install.sh
@@ -5,7 +5,8 @@ sudo apt-get upgrade -y
 sudo apt-get install emacs tree zsh -y
 
 # Installation et configuration de zsh / oh-my-zsh
-echo "cd" >> ~/.bashrc
 sh -c "$(wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
 cp slnz.zsh-theme ~/.oh-my-zsh/themes
 sed -i 's/ZSH_THEME=.*/ZSH_THEME="slnz"/' ~/.zshrc
+echo cd >> ~/.zshrc
+source ~/zshrc
